91f8a289589842b18008ea6fc99a9b3e5c4b39cc
Victor Medrado <victormedrado49@gmail.com>
2024-04-30 09:37:42 -0300

    Calendário e disponibilidade - Finalizando rotas de disponibilidade e carregando horarios disponiveis

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -56,5 +56,25 @@ export default async function handle(
     },
   )
 
-  return res.json({ possibleTimes })
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(), // greater than or equal
+        lt: referenceDate.set('hour', endHour).toDate(), // lower than or equal
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return res.json({ possibleTimes, availableTimes })
 }
